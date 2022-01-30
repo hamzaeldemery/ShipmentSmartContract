05a2ab9b6424d9f2bcf8c1333f0e6596e6114963
hamzaeldemery <hkhehmad@gmail.com>
2022-01-31 00:21:06 +0800

    Add verification status

--- client/src/Components/Admin/VerifyUser.js
+++ client/src/Components/Admin/VerifyUser.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { getCookie, verifyUser } from "../../helpers";
 
 export const VerifyUser = () => {
+    const [verified, setVerified] = useState(false);
+
     const handleOnSubmit = async (e) => {
+        setVerified(false);
         e.preventDefault();
         const res = await verifyUser(getCookie("username"), e.target[0].value);
+        if (res != false) {
+            setVerified(true);
+            setTimeout(() => {
+                setVerified(false);
+            }, 3000);
+        }
         console.log("res in admin ver user ---> ", res);
     };
 
     return (
         <div className="container">
-            <Form onSubmit={handleOnSubmit}>
-                <Form.Group>
-                    <Form.Label className="fw-bold ">User address</Form.Label>
-                    <Form.Control />
-                </Form.Group>
-                <Button type="submit" className="primary block w-100 my-4">
-                    Verify
-                </Button>
-            </Form>
+            {!verified ? (
+                <Form onSubmit={handleOnSubmit}>
+                    <Form.Group>
+                        <Form.Label className="fw-bold ">
+                            User address
+                        </Form.Label>
+                        <Form.Control />
+                    </Form.Group>
+                    <Button type="submit" className="primary block w-100 my-4">
+                        Verify
+                    </Button>
+                </Form>
+            ) : (
+                <div>
+                    <h1>Verified!</h1>
+                </div>
+            )}
         </div>
     );
 };
