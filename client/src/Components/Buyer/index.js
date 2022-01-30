a396d0d2381a073be2ba40aa4ef2e459aeb8d39f
hamzaeldemery <hkhehmad@gmail.com>
2022-01-31 00:00:24 +0800

    Buyer page

---
+++ client/src/Components/Buyer/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Row } from "react-bootstrap";
+import { Card } from "../Card";
+import { ShowShipment } from "../ShowShipment";
+import { PayShipment } from "./PayShipment";
+
+export const Buyer = () => {
+    const funcs = {
+        pay: {
+            title: "Pay Shipment",
+            comp: <PayShipment />,
+        },
+        showShipment: {
+            title: "Show Shipment",
+            comp: <ShowShipment />,
+        },
+    };
+
+    const [comp, setComp] = useState(null);
+    const [clicked, setClicked] = useState("");
+    return (
+        <div className="container">
+            <div className="container">
+                <Row xs={2} md={4} className="g-4">
+                    {Object.keys(funcs).map((key, val) => {
+                        return (
+                            <Card
+                                key={funcs[key].title}
+                                title={funcs[key].title}
+                                color={
+                                    clicked == funcs[key].title
+                                        ? "secondary"
+                                        : "dark"
+                                }
+                                onClick={() => {
+                                    if (clicked != funcs[key].title) {
+                                        setClicked(funcs[key].title);
+                                        setComp(funcs[key].comp);
+                                    } else {
+                                        setClicked("");
+                                        setComp(null);
+                                    }
+                                }}
+                            />
+                        );
+                    })}
+                </Row>
+            </div>
+            <div>{comp}</div>
+        </div>
+    );
+};
